91b40f151f8142df3bc1f4b5701476a04525f12c
Shema Arsene <aniyonkurushema@gmail.com>
2026-05-10 09:22:12 +0200

    Design recipePage (details page)

--- lib/pages/RecipePage.dart
+++ lib/pages/RecipePage.dart
@@ -20,7 +20,16 @@ class RecipePage extends StatelessWidget {
   }
 
   Widget _buildUI(BuildContext context) {
-    return Column(children: [_recipeImage(context), _recipeDetails()]);
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          _recipeImage(context),
+          _recipeDetails(context),
+          _recipeIngredients(context),
+          _recipeInstructions(context),
+        ],
+      ),
+    );
   }
 
   Widget _recipeImage(BuildContext context) {
@@ -36,7 +45,68 @@ class RecipePage extends StatelessWidget {
     );
   }
 
-  Widget _recipeDetails() {
-    return Container();
+  Widget _recipeDetails(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: MediaQuery.sizeOf(context).width,
+      padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "${recipe.cuisine}, ${recipe.difficulty}",
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
+          ),
+          Text(
+            recipe.name,
+            style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            "Rep time: ${recipe.prepTimeMinutes} Minutes | Cook time: ${recipe.cookTimeMinutes} Minutes",
+            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+          ),
+          Text(
+            "${recipe.rating.toString()} ⭐ | ${recipe.reviewCount} Reviews",
+            style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _recipeIngredients(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: MediaQuery.sizeOf(context).width,
+      padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+      child: Column(
+        children: recipe.ingredients.map((i) {
+          return Row(children: [const Icon(Icons.check_box), Text(" $i")]);
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _recipeInstructions(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: MediaQuery.sizeOf(context).width,
+      padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: recipe.instructions.map((i) {
+          return Text(
+            i,
+            maxLines: 3,
+            textAlign: TextAlign.start,
+            style: const TextStyle(fontSize: 15.0),
+          );
+        }).toList(),
+      ),
+    );
   }
 }
